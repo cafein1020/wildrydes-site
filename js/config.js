bd475094b7651e9a66c6d3fb60adf3d588ea2d7a
cafein1020 <test@gmail.com>
2023-03-05 11:53:11 +0900

    fix config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-northeast-1_6os5ehQHM', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3970vmskgdobki9s75vi4dg3cs', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
